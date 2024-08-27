df9a5e596cd752b7b60c1562e0a132349681f8f2
Jorge Hernandez Toledo <jorgeht2001@gmail.com>
2024-08-27 16:10:14 +0100

    First Deploy
    It works! Need to make some changes

--- scripts/pokeapi_fetch.js
+++ scripts/pokeapi_fetch.js
@@ -1,1 +1,52 @@
-let a = 1;
+let currentPokemonId = 1;
+const boton = document.getElementById('Encender');
+const pantalla = document.getElementById('pantalla');
+
+
+function fetchPokemonImage() {
+  
+  
+    // URL de la API para el primer Pokémon (Bulbasaur)
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${currentPokemonId}`;
+
+    // Realizar la solicitud fetch a la API
+    fetch(apiUrl)
+        .then(response => response.json())  // Convertir la respuesta a JSON
+        .then(data => {
+            // Obtener la URL de la imagen del Pokémon
+            const pokemonImageUrl = data.sprites.front_default;
+
+            // Mostrar la imagen en el elemento img
+            document.getElementById('pokemonImage').src = pokemonImageUrl;
+      
+        })
+        .catch(error => {
+            console.error('Error al obtener la imagen del Pokémon:', error);
+        });
+}
+
+function goNext() {
+
+  currentPokemonId++;
+
+  fetchPokemonImage();
+
+}
+function goBack() {
+
+  if(currentPokemonId == 1) {
+    alert("No hay pokemon anteriores a Bulbasaur");
+  } else {
+    currentPokemonId = currentPokemonId - 1;
+
+    fetchPokemonImage();
+  }
+
+  
+}
+
+
+
+boton.addEventListener('click', function() {
+  pantalla.style.backgroundImage = "url('https://img.freepik.com/vector-gratis/fondo-efecto-zoom-degradado-azul_23-2149762303.jpg?semt=ais_hybrid')";
+});
