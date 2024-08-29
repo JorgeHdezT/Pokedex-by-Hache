eaad0ca327ab11b5dce571dc069c35f6fd6e055a
Jorge Hernandez Toledo <jorgeht2001@gmail.com>
2024-08-29 18:28:18 +0100

    Id and Pokemon Type
    New features! Search button under maintenance

--- scripts/pokeapi_fetch.js
+++ scripts/pokeapi_fetch.js
@@ -6,24 +6,46 @@ let audio = document.getElementById("pokemonThemeAudio");
 
 
 function fetchPokemonImage() {
-  
-    // URL de la API para el primer Pokémon (Bulbasaur)
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${currentPokemonId}`;
 
-    // Realizar la solicitud fetch a la API
-    fetch(apiUrl)
-        .then(response => response.json())  // Convertir la respuesta a JSON
-        .then(data => {
-            // Obtener la URL de la imagen del Pokémon
-            const pokemonImageUrl = data.sprites.front_default;
+  // URL de la API para el primer Pokémon (Bulbasaur)
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${currentPokemonId}`;
 
-            // Mostrar la imagen en el elemento img
-            document.getElementById('pokemonImage').src = pokemonImageUrl;
-      
-        })
-        .catch(error => {
-            console.error('Error al obtener la imagen del Pokémon:', error);
-        });
+  // Realizar la solicitud fetch a la API
+  fetch(apiUrl)
+    .then(response => response.json())  // Convertir la respuesta a JSON
+    .then(data => {
+      // Obtener la URL de la imagen del Pokémon
+      const pokemonImageUrl = data.sprites.front_default;
+
+      // Mostrar la imagen en el elemento img
+      document.getElementById('pokemonImage').src = pokemonImageUrl;
+      document.getElementById('pokedexNumber').innerHTML = `N: ${currentPokemonId}`;
+
+    })
+    .catch(error => {
+      console.error('Error al obtener la imagen del Pokémon:', error);
+    });
+}
+
+function fetchPokemonType() {
+  // URL de la API para el primer Pokémon (Bulbasaur)
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${currentPokemonId}`;
+
+  // Realizar la solicitud fetch a la API
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Pokémon no encontrado');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const types = data.types.map(typeInfo => typeInfo.type.name);
+      document.getElementById('type').innerText = `${types.join(', ')}`;
+    })
+    .catch(error => {
+      document.getElementById('type').innerText = `Error: ${error.message}`;
+    });
 }
 
 function goNext() {
@@ -31,6 +53,7 @@ function goNext() {
   if (encendido == true) {
     currentPokemonId++;
     fetchPokemonImage();
+    fetchPokemonType();
   }
   else {
     alert("Enciende la consola");
@@ -39,12 +62,13 @@ function goNext() {
 function goBack() {
 
   if (encendido == true) {
-    if(currentPokemonId == 1) {
+    if (currentPokemonId == 1) {
       alert("No hay pokemon anteriores a Bulbasaur");
     } else {
       currentPokemonId = currentPokemonId - 1;
-  
+
       fetchPokemonImage();
+      fetchPokemonType();
     }
   }
 }
@@ -62,6 +86,7 @@ function EncenderGameboy() {
     playAudio();
     pantalla.style.backgroundImage = "url('https://img.freepik.com/vector-gratis/fondo-efecto-zoom-degradado-azul_23-2149762303.jpg?semt=ais_hybrid')";
     fetchPokemonImage();
+    fetchPokemonType();
 
   }
   else {
@@ -74,14 +99,21 @@ function apagarPantalla() {
   stopAudio();
   pantalla.style.backgroundImage = "";
   document.getElementById('pokemonImage').src = "";
+  document.getElementById("pokedexNumber").innerHTML = "";
+  document.getElementById("type").innerHTML = "";
   currentPokemonId = 1;
 }
 
 function playAudio() {
+  audio.volume = 0.1;
   audio.play();
 }
 
 function stopAudio() {
-  audio.pause();    
+  audio.pause();
   audio.currentTime = 0;
+}
+
+function buscar() {
+  alert("En desarrollo. Disculpe las molestias");
 }
